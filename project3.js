b5bfd340867dde1d038a7b5ba44954e47f57a0ea
Matthew Ko <matthew.ko@centre.edu>
2019-05-06 22:54:53 -0400

    3.1

--- project3.js
+++ project3.js
@@ -59,14 +59,15 @@ var startone = function(){
                     probs[4]-probs[0],probs[5]-probs[0],probs[6]-probs[0],probs[7]-probs[0],
                     probs[8]-probs[0],probs[9]-probs[0],0];
 
-  var svgwidth= window.innerWidth;
+
   var svgheight= window.innerHeight*0.9;
+  var svgwidth= window.innerWidth*0.50;
 
   var margins =
   {
     top:30,
     bottom:40,
-    left:10,
+    left:170,
     right:10
   }
 
@@ -135,7 +136,7 @@ var startone = function(){
              return 'rotate('+oscale(d)*180/Math.PI+',0,0)';
            });
 
-  var axname1 = ['Health','Marriage','Work 30 Hours Less','',
+  var axname1 = ['Good Health','Marriage','Work 30 Hours Less','',
                 '','','','','Income > 120,000','% Increase in Happiness'];
 
   var axname2 = ['30k-60k Income','60k-90k Income','90k-120k Income','','','','','',
@@ -271,7 +272,7 @@ var starttwo = function(){
 
   var menuyscale = d3.scaleLinear().domain([0,4]).range([0,height/1.5]);
 
-  var healthbuttons = [0,0.5,1];
+  var healthbuttons = [0,5,10];
   var incomebuttons = [0,1,2,3,4];
   var marriedbuttons = [0,1];
   var workhoursbuttons = [20,40,60,80,100];
@@ -280,6 +281,8 @@ var starttwo = function(){
   var buttonrowmake = function(data,row){
 
     menuPlot.selectAll('.button'+row).data(data).enter().append('circle')
+            .classed('button'+row,true)
+            .classed('clicked',false)
             .attr('data-name',function(d,i){
               return "name"+row+d;
             })
@@ -290,8 +293,81 @@ var starttwo = function(){
               return menuyscale(row)
             })
             .attr('r',8)
-            .attr('fill',d3.interpolatePuBu(0.3));
+            .attr('fill',d3.interpolatePuBu(0.3))
+            .on('click',function(d){
+
+              d3.selectAll('.button'+row).classed('clicked',false)
+                .attr('fill',d3.interpolatePuBu(0.3));
+              d3.select(this).classed('clicked',true);
+
+            })
+            .on('mouseover',function(d){
+              d3.select(this).style('cursor','pointer');
+
+            });
+
+    menuPlot.selectAll('.button'+row).each(function(d){
+      d3.select(this).classed('value'+row+d,true);
+    })
+
+    menuPlot.selectAll('.mentext'+row).data(data).enter().append('text')
+            .classed('menutext',true)
+            .attr('x',function(d,i){
+              return menuxscale1(i);
+            })
+            .attr('y',function(d,i){
+              return menuyscale(row)+(menuyscale(row+1)-menuyscale(row))/4
+            })
+            .text(function(d,i,ar){
+
+              if(ar.length==3){
+                if(d==0){return "Poor"}
+                else if(d==5){return "Average"}
+                else if(d==10){return "Great"};
+              }
+              else if(ar.length==2){
+                if(d==0){return "No"}
+                else if(d==1){return "Yes"}
+              }
+              else if(ar.length==5){
+                if(d==0){return "<30k"}
+                else if(d==1){return "30k-60k"}
+                else if(d==2){return "60k-90k"}
+                else if(d==3){return "90k-120k"}
+                else if(d==4){return ">120k"}
+                else{return d}
+              }
+              else{return "test"};
+
+            })
+            .attr("text-anchor", "middle")
+            .attr('fill',d3.interpolatePuBu(0.9));
+
+    menuPlot.append('text')
+            .attr('x',function(d){
+                return menuxscale1(0.5);
+            })
+            .attr('y',function(d){
+              return menuyscale(row)-(menuyscale(row+1)-menuyscale(row))/4
+            })
+            .classed('menutext',true)
+            .text(function(d){
+              if(row==0){
+                return "Health"
+              }
+              else if(row==2){
+                return 'Marriage'
+              }
+              else if(row==1){
+                return "Income"
+              }
+              else if(row==3){return "Working Hours (Including Overtime)"}
+              else if(row==4){return "Age"}
+              else{return "test"};
+            });
   }
+
+
 
 
    buttonrowmake(healthbuttons,0);
@@ -300,8 +376,179 @@ var starttwo = function(){
    buttonrowmake(workhoursbuttons,3);
    buttonrowmake(agebuttons,4);
 
+   menuPlot.append('rect').attr('x',menuxscale1(0)).attr('y',menuyscale(5))
+           .classed('create',true)
+           .attr('width',80).attr('height',30)
+           .attr('fill',d3.interpolatePuBu(0.3))
+           .on('click',function(d){
+             d3.selectAll('.create').transition(500)
+               .attr('fill','black').on('end',function(d){
+                 d3.selectAll('.create').transition(500)
+                   .attr('fill',d3.interpolatePuBu(0.3));
+               });
+              var newper = []
+              if(d3.select('.value00').classed('clicked')){newper.push(0)}
+              else if (d3.select('.value05').classed('clicked')) {newper.push(0.5)}
+              else if (d3.select('.value010').classed('clicked')) {newper.push(1)}
+              else{newper.push(0)};
+
+              if(d3.select('.value20').classed('clicked')){newper.push(0)}
+              else if (d3.select('.value21').classed('clicked')) {newper.push(1)}
+              else{newper.push(0)};
+
+              if(d3.select('.value320').classed('clicked')){newper.push(20)}
+              else if (d3.select('.value340').classed('clicked')) {newper.push(40)}
+              else if (d3.select('.value360').classed('clicked')) {newper.push(60)}
+              else if (d3.select('.value380').classed('clicked')) {newper.push(80)}
+              else if (d3.select('.value3100').classed('clicked')) {newper.push(100)}
+              else{newper.push(0)};
+
+              if(d3.select('.value420').classed('clicked')){newper.push(20)}
+              else if (d3.select('.value440').classed('clicked')) {newper.push(40)}
+              else if (d3.select('.value460').classed('clicked')) {newper.push(60)}
+              else if (d3.select('.value480').classed('clicked')) {newper.push(80)}
+              else if (d3.select('.value4100').classed('clicked')) {newper.push(100)}
+              else{newper.push(20)};
+
+              if(d3.select('.value10').classed('clicked')){newper.push(0)}
+              else if (d3.select('.value11').classed('clicked')) {newper.push(1)}
+              else if (d3.select('.value12').classed('clicked')) {newper.push(2)}
+              else if (d3.select('.value13').classed('clicked')) {newper.push(3)}
+              else if (d3.select('.value14').classed('clicked')) {newper.push(4)}
+              else{newper.push(0)};
+
+              console.log(newper);
+
+              var mockind = newind(newper[0],newper[1],newper[2],newper[3],newper[4]);
+
+              console.log(mockind);
+
+              createInd(mockind);
+           })
+           .on('mouseover',function(d){
+             d3.select(this).style('cursor','pointer');
+
+           });
+
+   menuPlot.append('rect').attr('x',menuxscale1(2)).attr('y',menuyscale(5))
+           .classed('remove',true)
+           .attr('width',80).attr('height',30)
+           .attr('fill',d3.interpolatePuBu(0.3))
+           .on('click',function(d){
+             d3.selectAll('.remove').transition(500)
+               .attr('fill','black').on('end',function(d){
+                 d3.selectAll('.remove').transition(500)
+                   .attr('fill',d3.interpolatePuBu(0.3));
+               });
+
+             compareplot.selectAll('.individual').transition().style('opacity',0 )
+               .remove();
+
+             compareplot.selectAll('.percentline').transition().style('opacity',0).remove();
+
+             compareplot.selectAll('.percentnum').transition().style('opacity',0).remove(); 
+           })
+           .on('mouseover',function(d){
+             d3.select(this).style('cursor','pointer');
+
+           });
+
+  menuPlot.append('text').attr('x',menuxscale1(0)+(menuxscale1(1)+menuxscale1(0))/2).attr('y',menuyscale(5)+(menuyscale(5)-menuyscale(4))/2)
+          .text('New Individual')
+          .attr('text-anchor','middle')
+          .style('font-family','Advent Pro')
+          .attr('fill',d3.interpolatePuBu(0.9));
+
+  menuPlot.append('text').attr('x',menuxscale1(2)+(menuxscale1(1)+menuxscale1(0))/2).attr('y',menuyscale(5)+(menuyscale(5)-menuyscale(4))/2)
+          .text('Remove All')
+          .attr('text-anchor','middle')
+          .style('font-family','Advent Pro')
+          .attr('fill',d3.interpolatePuBu(0.9))
+
+  return d3.select('.value00').classed('clicked');
+
 }
 
+var createInd = function(individual){
+
+  var svgwidth= window.innerWidth;
+  var svgheight= window.innerHeight*0.9;
+
+  var margins =
+  {
+    top:100,
+    bottom:10,
+    left:30,
+    right:50
+  }
+
+  var marginmenu = {
+    left:300,
+    right:10
+
+
+  }
+
+  var width = svgwidth -margins.left - margins.right;
+  var height = svgheight -margins.top - margins.bottom;
+
+  var compareplot = d3.select('.compare');
+
+  var happyYscale = d3.scaleLinear().domain([0,1]).range([height,0]);
+
+  compareplot.append('circle')
+             .attr('cx',function(d){
+               return (width-marginmenu.left)/2;
+             })
+             .attr('cy',function(d){
+               return happyYscale(prob(individual))
+             })
+             .attr('r',10)
+             .classed('individual',true)
+             .style('opacity',0)
+             .style('fill',function(d){
+
+               return d3.interpolateRdYlGn(prob(individual));
+             })
+             .style('stroke-width',1)
+             .style('stroke','black');
+
+  var round = d3.format('.3n');
+
+
+  compareplot.append('text')
+            .attr('x',function(d){
+              return 100;
+            })
+            .attr('y',function(d){
+              return happyYscale(prob(individual))+6
+            })
+            .text(function(d){
+              return round(prob(individual)*100)+'%';
+            })
+            .classed('menutext',true)
+            .classed('percentnum',true)
+            .style('opacity',0);
+
+
+  var linegen = d3.line().x(function(d){return d.x;}).y(function(d){return d.y;});
+
+  compareplot.append('path')
+             .attr('d',linegen(
+               [{x:(width-marginmenu.left)/2-20,y:happyYscale(prob(individual))},
+                {x:160,y:happyYscale(prob(individual))}]))
+             .classed('percentline',true)
+             .style('stroke-width',0.7)
+             .style('stroke-dasharray',('2','20'))
+             .attr('stroke','black')
+             .style('opacity',0);
+
+  compareplot.selectAll('.individual').transition().duration(750).style('opacity',0.7);
+  compareplot.selectAll('.percentline').transition().duration(500).style('opacity',1);
+  compareplot.selectAll('.percentnum').transition().duration(250).style('opacity',1);
+
+  console.log(compareplot.classed('compare'));
+}
 
 //startone();
 console.log(startone());
